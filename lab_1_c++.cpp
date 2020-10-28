36656a28d86bcce1c4dcfd6d8dd1b2f19920d8f2
Snakeheart404 <71406408+Snakeheart404@users.noreply.github.com>
2020-10-28 02:10:05 +0200

    Add files via upload

---
+++ lab_1_c++.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; //тризначне число
+    int first_n; //перша цифра зліва
+    int res; //число з приписаною першою цифрою в кінці
+
+    cout << "Enter a three-digit number:" << endl;
+    cin >> n;
+
+    first_n = n / 100;
+    n = n % 100;
+    res = n * 10 + first_n;
+
+    cout << "Result: " << res << endl;
+
+    system("pause");
+}
