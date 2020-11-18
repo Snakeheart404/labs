ac8efd840a49eb3d05d2a23d2272fe53813fa9d5
Snakeheart404 <71406408+Snakeheart404@users.noreply.github.com>
2020-11-18 11:38:53 +0200

    Add files via upload

---
+++ lab 4 c++.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n; //Кількість коренів n
+    double x; //Поточне значення виразу
+    double Res; //Шукане значення виразу з n коренів
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    x = 0.0;
+
+    for (int i = 0; i < n; i++)
+    {
+        x = sqrt(2 + x);
+    }
+
+    Res = x;
+
+    cout << "Result: " << Res << endl;
+
+    system("pause");
+
+}
+
