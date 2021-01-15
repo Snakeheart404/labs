49061140e214f59c40a6f00030e398870c9bb2c5
Snakeheart404 <71406408+Snakeheart404@users.noreply.github.com>
2021-01-15 09:04:02 +0200

    Add files via upload

---
+++ lab7.cpp
@@ -0,0 +1,85 @@
+﻿#include<iostream>
+
+using namespace std;
+
+//=========Прототипи функцій============
+void fillArrayA(char A[], int number); // прототип функції, що заповнює перший масив
+void fillArrayB(char B[], int number); // прототип функції, що заповнює другий масив
+void outArray(char A[], int number); // прототип функції для виведення масивів
+void makeArrayC(char A[], char B[], char C[], int number, int* size); // прототип функції, що шукає спільні елементи перших двох масивів і заповнює ними третій масив
+double arithmeticMean(char C[], int* size); // прототип функції для знаходження середнього арифметичного елементів третього масиву, коди яких більше 55
+
+//=========Головна функція============
+int main() {
+    int const size = 10; // розмір першого та другого масивів
+    char A[size], B[size]; // перший і другий масив
+    char C[10]; // третій масив
+    int C_size = 0; // розмір третього масиву 
+    int* pC_size = &C_size; // покажчик на розмір третього масиву 
+    fillArrayA(A, size);
+    fillArrayB(B, size);
+    cout << "First array: " << endl;
+    outArray(A, size);
+    cout << "Second array: " << endl;
+    outArray(B, size);
+    makeArrayC(A, B, C, size, pC_size);
+    cout << "Third array: "<< endl;
+    outArray(C, C_size);
+    double mean = arithmeticMean(C, pC_size);
+
+    cout << "Arithmetic mean: " << mean;
+
+
+    return 0;
+}
+
+//=========Визначення функцій============
+
+void fillArrayA(char A[], int number) // функція, що заповнює перший масив
+{
+    for (int i = 0; i < number; i++) {
+        A[i] = char(45 + 2 * i);
+    }
+}
+
+void fillArrayB(char B[], int number) // функція, що заповнює другий масив
+{
+    for (int i = 0; i < number; i++) {
+        B[i] = char(61 - 2 * i);
+    }
+}
+
+void outArray(char A[], int number) // функція для виведення масивів
+{
+    for (int i = 0; i < number; i++) {
+        cout << A[i] << endl;
+    }
+}
+
+void makeArrayC(char A[], char B[], char C[], int number, int* size) // функція, що шукає спільні елементи перших двох масивів і заповнює ними третій масив
+{
+    for (int j = 0; j < number; j++) {
+        for (int k = 0; k < number; k++) {
+            if (A[j] == B[k])
+            {
+                C[*size] = A[j];
+                *size += 1;
+            }
+        }
+    }
+}
+
+double arithmeticMean(char C[], int* size) // функція для знаходження середнього арифметичного елементів третього масиву, коди яких більше 55
+{
+    int counter = 0;
+    int sum = 0;
+    for (int i = 0; i < *size; i++) {
+        if (C[i] > 55) {
+            sum += C[i]; // сума елементів масиву, коди яких більше 55
+            counter++; // кількість елементів масиву, коди яких більше 55
+        }
+
+    }
+    double result = double(sum) / double(counter); // здійснюється явне приведення типів, ми не можемо працювати з цілими числами, бо заздалегідь не знаємо, чи буде середнє арифметичне цілим числом
+    return result;
+}
---
+++ lab8_c++.cpp
@@ -0,0 +1,103 @@
+﻿#include<iostream>
+
+#include <cstdlib>
+
+#include <ctime>
+
+#include <iomanip>
+
+using namespace std;
+
+import array;
+
+//=========Прототипи функцій============
+void fillMatrix(int matrix[n][m], int n, int m); // прототип функції для ініціювання двовимірного масиву (матриці n х m) випадковими числами
+void outputMatrix(int matrix[n][m], int n, int m); // прототип функції для виведення двовимірного масиву (матриці) 
+void fillAray(int matrix[n][m], int array[], int n, int m); //прототип  функції для ініціювання одновимірного масиву максимальними значеннями елементів стовпців двовимірного масиву
+void outArray(int array[], int m); //прототип  функції для виведення одновимірного масиву
+void bubbleSort(int array[], int m); //прототип  функції для сортування одновимірного масиву обміном (бульбашкою) за спаданням                     
+
+//=========Головна функція============
+int main() {
+
+    int n; // кількість рядків і стовпчиків матриці
+    cout << "Enter the size of matrix: "; 
+    cin >> n;
+    int** matrix = generateMatrix(n); // двовимірний масив (матриця)
+    fillMatrix(matrix, n, m);
+    cout << "Matrix: " << endl;
+    outputMatrix(matrix, n, m);
+    cout << endl;
+
+    int array[m]; // одновимірний масив
+    fillAray(matrix, array, n, m);
+    cout << "Unsorted array: " << endl;
+    outArray(array, m);
+    cout << endl;
+    bubbleSort(array, m);
+    cout << "Sorted array: " << endl;
+    outArray(array, m);
+    cout << endl;
+
+    system("pause");
+}
+
+//=========Визначення функцій============
+int** generateMatrix(int n) {
+
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    } // функція для ініціювання двовимірного масиву (матриці n х m) випадковими числами
+{
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix[i][j] = rand();
+        }
+    }
+}
+
+void outputMatrix(int matrix[n][m], int n, int m) // функція для виведення двовимірного масиву (матриці) 
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << setw(5) << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void fillAray(int matrix[n][m], int array[], int n, int m) // функція для ініціювання одновимірного масиву максимальними значеннями елементів стовпців двовимірного масиву
+{
+    for (int i = 0; i < m; i++) {
+        int maxEl = matrix[0][i];
+        for (int j = 1; j < n; j++) {
+            if (matrix[j][i] > maxEl) maxEl = matrix[j][i];
+        }
+        array[i] = maxEl;
+    }
+}
+
+void bubbleSort(int array[], int m) //функція для сортування одновимірного масиву обміном (бульбашкою) за спаданням
+{
+    int tmp;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            if (array[j + 1] > array[j]) {
+                tmp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+
+}
+
+void outArray(int array[], int m) // функція для виведення одновимірного масиву
+{
+    for (int i = 0; i < m; i++) {
+        cout << array[i] << endl;
+    }
+}
+
